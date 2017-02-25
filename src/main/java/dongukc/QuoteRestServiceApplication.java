469424922c189ae93514bbc484b03c4d35df14a2
Ubuntu <ubuntu@ip-172-30-3-59.ec2.internal>
2017-02-25 16:16:01 +0000

    service instances controller added

--- src/main/java/dongukc/QuoteRestServiceApplication.java
+++ src/main/java/dongukc/QuoteRestServiceApplication.java
@@ -1,6 +1,16 @@
 package dongukc;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
@@ -11,4 +21,17 @@ public class QuoteRestServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QuoteRestServiceApplication.class, args);
 	}
+}
+
+@RestController
+class ServiceInstanceRestController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
 }
